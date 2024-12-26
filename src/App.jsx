d6e46cf8ceb4ad4a30453d45dc8e21e3fb56cfdc
Debasis Mishra <debasismishra000123@gmail.com>
2024-12-26 11:03:12 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [question, setquestion] = useState("");
+
   async function generateAnswer() {
     console.log("Loading");
     const response = await axios({
@@ -12,18 +15,29 @@ function App() {
           {
             parts: [
               {
-                text: "Explain how AI works",
+                text: question,
               },
             ],
           },
         ],
       },
     });
-    console.log(response);
+    console.log(
+      response["data"]["candidates"][0]["content"]["parts"][0]["text"]
+    );
   }
   return (
     <>
       <h1>AI CHATBOT</h1>
+      <textarea
+        value={question}
+        onChange={(e) => setquestion(e.target.value)}
+        name=""
+        id=""
+        cols={30}
+        rows={10}
+      ></textarea>
+      <br />
       <button onClick={generateAnswer}>generate Answer</button>
     </>
   );
